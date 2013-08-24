0004eb54e2a40490f4018027a943073fb0aa960e
Alejandro Ruiz <prueba@toonazo.com>
2013-08-24 02:39:04 -0500

    primera version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nirvana::Application.config.secret_token = '095f0430eff7752406d249518979aac6541d71381d17da22c8d4c1feace660f95437f6f43309e44dfb8744e3c7ed31a97316270043441573070a57ee7c7be109'
